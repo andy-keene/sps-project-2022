2ab5a1909f10ec98ecf6734433bdbd7e723b29a3
Matthew Wong <matthew.yt.wong@gmail.com>
2022-07-10 23:40:19 +0000

    Update Resource object

--- project/src/main/java/com/google/sps/servlets/Resource.java
+++ project/src/main/java/com/google/sps/servlets/Resource.java
@@ -4,15 +4,19 @@ public class Resource {
     private final Organizer organizer;
     private final int id;
     private final String eventName;
+    private final String eventDate;
+    private final String[] ageGroup;
     private final String location;
     private final String link;
     private final String description;
     private final String[] ethnicity;
 
-    public Resource(Organizer organizer, int id, String eventName, String location, String link, String description, String[] ethnicity) {
+    public Resource(Organizer organizer, int id, String eventName, String eventDate, String[] ageGroup, String location, String link, String description, String[] ethnicity) {
         this.organizer = organizer;
         this.id = id;
         this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.ageGroup = ageGroup;
         this.location = location;
         this.link = link;
         this.description = description;
