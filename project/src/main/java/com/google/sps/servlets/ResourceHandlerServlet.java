55590101a62dd860411aeeba73a7d5302a942074
Matthew Wong <matthew.yt.wong@gmail.com>
2022-07-21 23:49:00 +0000

    Fix form parsing to include arrays

--- project/src/main/java/com/google/sps/servlets/ResourceHandlerServlet.java
+++ project/src/main/java/com/google/sps/servlets/ResourceHandlerServlet.java
@@ -27,40 +27,62 @@ public class ResourceHandlerServlet extends HttpServlet {
         String location = request.getParameter("inputEventLocation");
         String link = request.getParameter("inputLink");
         String description = request.getParameter("inputDesciption");
-        String ethnicity = request.getParameter("white");
-
-        String nine = request.getParameter("gradeNine");
 
         // Create array for age groups
         List<String> ageGroup = new ArrayList<>();
 
-        ageGroup.add(request.getParameter("gradeNine"));
-
-        if (request.getParameter("gradeNine") == "Grade 9") {
+        if (request.getParameter("gradeNine") != null) {
             ageGroup.add("Grade 9");
         }
-        if (request.getParameter("gradeTen") == "Grade 10") {
+        if (request.getParameter("gradeTen") != null) {
             ageGroup.add("Grade 10");
         }
-        if (request.getParameter("gradeEleven") == "Grade 11") {
+        if (request.getParameter("gradeEleven") != null) {
             ageGroup.add("Grade 11");
         }
-        if (request.getParameter("gradeTwelve") == "Grade 12") {
+        if (request.getParameter("gradeTwelve") != null) {
             ageGroup.add("Grade 12");
         }
-        if (request.getParameter("freshman") == "Freshman") {
+        if (request.getParameter("freshman") != null) {
             ageGroup.add("Freshman");
         }
-        if (request.getParameter("sophomore") == "Sophomore") {
+        if (request.getParameter("sophomore") != null) {
             ageGroup.add("Sophomore");
         }
-        if (request.getParameter("junior") == "Junior") {
+        if (request.getParameter("junior") != null) {
             ageGroup.add("Junior");
         }
-        if (request.getParameter("senior") == "Senior") {
+        if (request.getParameter("senior") != null) {
             ageGroup.add("Senior");
         }
         
+        // Create array for ethnicities
+        List<String> ethnicities = new ArrayList<>();
+
+        if (request.getParameter("white") != null) {
+            ethnicities.add("White");
+        }
+        if (request.getParameter("black") != null) {
+            ethnicities.add("Black");
+        }
+        if (request.getParameter("hispanic") != null) {
+            ethnicities.add("Hispanic");
+        }
+        if (request.getParameter("asian") != null) {
+            ethnicities.add("Asian");
+        }
+        if (request.getParameter("americanIndian") != null) {
+            ethnicities.add("American Indian or Alaska Native");
+        }
+        if (request.getParameter("middleEastern") != null) {
+            ethnicities.add("Middle Eastern");
+        }
+        if (request.getParameter("twoOrMore") != null) {
+            ethnicities.add("2 or more");
+        }
+        if (request.getParameter("notToAnswer") != null) {
+            ethnicities.add("Prefer not to answer");
+        }
 
         // Print the value so you can see it in the server logs.
         System.out.println("You submitted: " + organizerName);
@@ -70,45 +92,43 @@ public class ResourceHandlerServlet extends HttpServlet {
         System.out.println("You submitted: " + location);
         System.out.println("You submitted: " + link);
         System.out.println("You submitted: " + description);
-        System.out.println("You submitted: " + nine);
-
-        // System.out.println("You submitted: " + ageGroup);
 
         // Datastore code
-        // Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-        // KeyFactory keyFactory = datastore.newKeyFactory().setKind("Resource");
-        // FullEntity taskEntity =
-        //     Entity.newBuilder(keyFactory.newKey())
-        //         .set("organizerName", organizerName)
-        //         .set("organizerEmail", organizerEmail)
-        //         .set("eventName", eventName)
-        //         .set("eventDate", eventDate)
-        //         .set("location", location)
-        //         .set("link", link)
-        //         .set("description", description)
-        //         .set("ethnicity", ethnicity)
-        //         .build();
-        // datastore.put(taskEntity);
-        // response.sendRedirect("/index.html");
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Resource");
+        FullEntity taskEntity =
+            Entity.newBuilder(keyFactory.newKey())
+                .set("organizerName", organizerName)
+                .set("organizerEmail", organizerEmail)
+                .set("eventName", eventName)
+                .set("eventDate", eventDate)
+                .set("location", location)
+                .set("link", link)
+                .set("description", description)
+                // .set("ageGroup", ageGroup)
+                // .set("ethnicities", ethnicities)
+                .build();
+        datastore.put(taskEntity);
+        response.sendRedirect("/start-page.html");
 
         // Write the value to the response so the user can see it
         response.setContentType("text/html;");
-        response.getWriter().println("You submitted: " + organizerName);
-        response.getWriter().println("You submitted: " + organizerEmail);
-        response.getWriter().println("You submitted: " + eventName);
-        response.getWriter().println("You submitted: " + eventDate);
-        response.getWriter().println("You submitted: " + location);
+        response.getWriter().println("Organizer name: " + organizerName);
+        response.getWriter().println("Organizer email: " + organizerEmail);
+        response.getWriter().println("Event name: " + eventName);
+        response.getWriter().println("Event date: " + eventDate);
+        response.getWriter().println("Location: " + location);
+        response.getWriter().println("Location: " + link);
+        response.getWriter().println("Location: " + description);
 
-        response.getWriter().println("You submitted: " + nine);
-        response.getWriter().println("You submitted: " + ageGroup.get(0));
+        response.getWriter().println("ageGroup count: " + ageGroup.size());
+        response.getWriter().println("ethnicities count: " + ethnicities.size());
 
-        // for (int i = 0; i < ageGroup.size(); i++) {
-        //     response.getWriter().println("You submitted: " + ageGroup.get(1));
-        // }
-        // response.getWriter().println("You submitted: " + ageGroup.get(1));
-        // response.getWriter().println("You submitted: " + ageGroup.get(2));
-        // response.getWriter().println("You submitted: " + ageGroup.get(0));
-
-        response.getWriter().println("You submitted: " + ethnicity);
+        for (int i = 0; i < ageGroup.size(); i++) {
+            response.getWriter().println("AgeGroup: " + ageGroup.get(i));
+        }
+        for (int i = 0; i < ethnicities.size(); i++) {
+            response.getWriter().println("Ethnicity: " + ethnicities.get(i));
+        }
     }
 }
