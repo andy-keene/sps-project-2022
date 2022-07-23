964270850cce7d384a2d4b0bca86debf5a0e75be
Matthew Wong <matthew.yt.wong@gmail.com>
2022-07-23 16:35:00 +0000

    Temporary commits

--- project/src/main/java/com/google/sps/servlets/Resource.java
+++ project/src/main/java/com/google/sps/servlets/Resource.java
@@ -1,18 +1,21 @@
 package com.google.sps.servlets;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Resource {
     private final String organizerName;
     private final String organizerEmail;
     private final long id;
     private final String eventName;
     private final String eventDate;
-    private final String[] ageGroup;
+    private final List<String> ageGroup;
     private final String location;
     private final String link;
     private final String description;
-    private final String[] ethnicity;
+    private final List<String> ethnicity;
 
-    public Resource(String organizerName, String organizerEmail, long id, String eventName, String eventDate, String[] ageGroup, String location, String link, String description, String[] ethnicity) {
+    public Resource(String organizerName, String organizerEmail, long id, String eventName, String eventDate, List<String> ageGroup, String location, String link, String description, List<String> ethnicity) {
         this.organizerName = organizerName;
         this.organizerEmail = organizerEmail;
         this.id = id;
--- project/src/main/java/com/google/sps/servlets/ResourceListServlet.java
+++ project/src/main/java/com/google/sps/servlets/ResourceListServlet.java
@@ -11,6 +11,7 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,17 +27,22 @@ public class ResourceListServlet extends HttpServlet {
                 .build();
         QueryResults<Entity> results = datastore.run(query);
 
+        List<String> temp = new ArrayList<>(Arrays.asList("String", "String2"));
+
         List<Resource> formResponses = new ArrayList<>();
         while (results.hasNext()) {
             Entity entity = results.next();
 
-            String id = entity.getString("organizerName");
-            // String title = entity.getString("title");
-            // long timestamp = entity.getLong("timestamp");            
-
-            // TODO: use in case this code is helpful
-            // create new resource here once JS from front-end is done
-            // add resource object to arrayList
+            long id = entity.getKey().getId();
+            String organizerName = entity.getString("organizerName");
+            String organizerEmail = entity.getString("organizerEmail");
+            String eventName = entity.getString("eventName");
+            String eventDate = entity.getString("eventDate");
+            String location = entity.getString("location");
+            String link = entity.getString("link");
+            String description = entity.getString("description");
+            List<String> ageGroup = entity.getValue("ageGroup");
+            List<String> ethnicities = entity.getValue("description");
 
             // Task task = new Task(id, title, timestamp);
             // tasks.add(task);
